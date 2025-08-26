804f08847b57bc2857537c8d2a3b1d737e08b428
ren <122001908+chakachuu@users.noreply.github.com>
2025-08-26 07:23:10 +0400

    Update site.jsx

--- site.jsx
+++ site.jsx
@@ -1,5 +1,5 @@
 import React from "https://esm.sh/react@18";
 import { createRoot } from "https://esm.sh/react-dom@18/client";
-import Site from "https://chakachuu.github.io/resume/site-impl.jsx";
+import Site from "./site-impl.jsx";
 
 createRoot(document.getElementById("root")).render(React.createElement(Site));
